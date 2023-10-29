55dc8aac60b2924aa9d7e383f5212057feb700f8
DumanBruh <dmarlambekov@gmail.com>
2023-10-29 19:29:15 +0600

    feature: create pdf, docx parser

--- src/telegram/parser/parser.service.ts
+++ src/telegram/parser/parser.service.ts
@@ -1,4 +1,49 @@
 import { Injectable } from "@nestjs/common";
+import * as pdfjs from "pdfjs-dist";
+import mammoth from "mammoth";
+import axios from "axios";
+import * as textract from "textract";
+
+type PDFSource = Buffer | string;
 
 @Injectable()
-export class ParserService {}
+export class ParserService {
+  async extractTextFromPDF(source: PDFSource): Promise<string> {
+    const doc = await pdfjs.getDocument(source).promise;
+    let pageTexts = Array.from({ length: doc.numPages }, async (v, i) => {
+      return (await (await doc.getPage(i + 1)).getTextContent()).items
+        .map((token) => {
+          if ("str" in token) {
+            return token.str;
+          } else {
+            return "";
+          }
+        })
+        .join("");
+    });
+    return (await Promise.all(pageTexts)).join("");
+  }
+
+  async extractTextFromDocx(url: string): Promise<string> {
+    const response = await axios.get(url, { responseType: "arraybuffer" });
+    const buffer = Buffer.from(response.data, "utf-8");
+    const result = await mammoth.extractRawText({ buffer: buffer });
+    return result.value;
+  }
+
+  // async extractTextFromDoc(url: string): Promise<string> {
+  //   return new Promise((resolve, reject) => {
+  //     fetch(url).then(async (res) => {
+  //       const buffer = await res.buffer();
+  //       textract.fromBufferWithMime(
+  //         "application/msword",
+  //         buffer,
+  //         (error, text) => {
+  //           if (error) reject(error);
+  //           else resolve(text);
+  //         }
+  //       );
+  //     });
+  //   });
+  // }
+}
--- src/telegram/scenes/briefly-scene/briefly.module.ts
+++ src/telegram/scenes/briefly-scene/briefly.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
 import { BrieflyScene } from "./briefly.scene";
+import { ParserService } from "@/telegram/parser/parser.service";
 
 @Module({
-    providers: [BrieflyScene]
+  providers: [BrieflyScene, ParserService],
 })
 export class BrieflyModule {}
